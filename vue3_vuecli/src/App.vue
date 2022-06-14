28802547fc99690513eb00bbf2d0e4195229ce22
camphorswood <2191964232@qq.com>
2022-06-14 17:18:57 +0800

    1
    1

--- vue3_vuecli/src/App.vue
+++ vue3_vuecli/src/App.vue
@@ -1,40 +1,63 @@
 <template>
   <!--  Vue3 组件中的模板结构，可以没有根标签-->
-  <h1>姓名:{{ name }}</h1>
-  <h1>年龄:{{ age }}</h1>
-  <h1>工作类型:{{ job.type }}</h1>
-  <h1>工作薪水:{{ job.salary }}</h1>
+  <!--  ref-->
+  <!--  <h1>姓名:{{ name }}</h1>-->
+  <!--  <h1>年龄:{{ age }}</h1>-->
+  <!--  <h1>工作类型:{{ job.type }}</h1>-->
+  <!--  <h1>工作薪水:{{ job.salary }}</h1>-->
+  <!-- reactive -->
+  <h1>姓名:{{ person.name }}</h1>
+  <h1>年龄:{{ person.age }}</h1>
+  <h1>工作类型:{{ person.job.type }}</h1>
+  <h1>工作薪水:{{ person.job.salary }}</h1>
   <button @click="changeInfo">点击</button>
 </template>
 
 <script>
-import {ref} from "vue";
+import {
+  // ref,
+  reactive
+} from "vue";
 
 export default {
   name: 'App',
   components: {},
   setup() {
     //  数据
-    let name = ref('张三')
-    let age = ref(18)
-    let job = ref({
-      type: '前端工程师',
-      salary: '30K'
-    })
+    // let name = ref('张三')
+    // let age = ref(18)
+    // let job = ref({
+    //   type: '前端工程师',
+    //   salary: '30K'
+    // })
 
     //  函数
+    // function changeInfo() {
+    //   name.value = '李四'
+    //   age.value = 68
+    //   job.value.type = '全栈工程师'
+    //   job.value.salary = '60K'
+    // }
+
+    let person = reactive({
+      name: '张三',
+      age: 18,
+      job: {
+        type: '前端工程师',
+        salary: '30K'
+      }
+    })
+
     function changeInfo() {
-      name.value = '李四'
-      age.value = 68
-      job.value.type = '全栈工程师'
-      job.value.salary = '60K'
+      person.name = '李四'
+      person.age = 68
+      person.job.type = '全栈工程师'
+      person.job.salary = '60K'
     }
 
     return {
-      changeInfo,
-      name,
-      age,
-      job
+      person,
+      changeInfo
     }
   }
 }
