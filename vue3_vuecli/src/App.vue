c8cb67ae39d56cf6c20fa5da641a27eca9ff433c
camphorswood <2191964232@qq.com>
2022-06-14 10:52:15 +0800

    初始化
    初始化

---
+++ vue3_vuecli/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <!--  Vue3 组件中的模板结构，可以没有根标签-->
+  <h1>姓名:{{ name }}</h1>
+  <h1>年龄:{{ age }}</h1>
+  <h1>工作类型:{{ job.type }}</h1>
+  <h1>工作薪水:{{ job.salary }}</h1>
+  <button @click="changeInfo">点击</button>
+</template>
+
+<script>
+import {ref} from "vue";
+
+export default {
+  name: 'App',
+  components: {},
+  setup() {
+    //  数据
+    let name = ref('张三')
+    let age = ref(18)
+    let job = ref({
+      type: '前端工程师',
+      salary: '30K'
+    })
+
+    //  函数
+    function changeInfo() {
+      name.value = '李四'
+      age.value = 68
+      job.value.type = '全栈工程师'
+      job.value.salary = '60K'
+    }
+
+    return {
+      changeInfo,
+      name,
+      age,
+      job
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
